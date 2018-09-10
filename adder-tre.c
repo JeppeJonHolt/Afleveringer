25b3d5c4111121441f36d245748ff5a571128d03
Jeppe <jeppe@bandsholm.com>
2018-09-10 19:53:10 +0200

    Add files via upload

---
+++ adder-tre.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+    int a,b,c;
+    int d;
+printf("Dette program, vil pluse tre tal sammen: \n");
+printf("skriv dine to tal med et mellemrum her: \n");
+
+scanf("%d %d %d",&a, &b, &c);
+printf("Resultat \n");
+d = (a+b+c);
+printf("%d \n",d);
+
+}
